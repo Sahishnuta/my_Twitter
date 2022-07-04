5df8daa7ba0c7462f8487d77fb5bbbf0e771d0c6
Sahishnuta <50331193+Sahishnuta@users.noreply.github.com>
2022-07-04 20:50:11 +0200

    Add files via upload

--- server/scripts/deploy.js
+++ server/scripts/deploy.js
@@ -1,3 +1,5 @@
+const {ethers} = require("hardhat");
+
 const main = async() => {
     const contractFactory = await ethers.getContractFactory('TwitterContract');
     const contract = await contractFactory.deploy();
